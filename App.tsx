052ac8c003820330e0962f9a5be094b462e5465a
Marcelo Bracet <marcelo.bracet1@gmail.com>
2024-04-29 22:17:40 -0300

    feat: first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [mainState, setMainState] = useState<"read" | "write">("read");
+
+  {
+    return mainState === "read" ? (
+      <View style={styles.container}>
+        <Text>Bracet Notes</Text>
+        <StatusBar style="dark" />
+      </View>
+    ) : (
+      <View style={styles.container}>
+        <Text>Write Note</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
