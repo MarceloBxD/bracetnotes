e53b4b91b1847e6c3a965d48802892faa2b135fa
Marcelo Bracet <marcelo.bracet1@gmail.com>
2024-04-29 22:43:56 -0300

    Add NativeWind types and update tsconfig.json

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Button } from "./components/Button";
 
 export default function App() {
   const [mainState, setMainState] = useState<"read" | "write">("read");
 
   {
     return mainState === "read" ? (
-      <View style={styles.container}>
+      <View className="" style={styles.container}>
         <Text>Bracet Notes</Text>
+        <Button onPress={() => setMainState("write")} label="Add Item" />
         <StatusBar style="dark" />
       </View>
     ) : (
       <View style={styles.container}>
         <Text>Write Note</Text>
-        <StatusBar style="auto" />
+        <StatusBar style="dark" />
       </View>
     );
   }
