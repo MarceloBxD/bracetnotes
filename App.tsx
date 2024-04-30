fcbed20fbc779f20949a2d0485d2e62335393979
Marcelo Bracet <marcelo.bracet1@gmail.com>
2024-04-30 00:29:06 -0300

    Update App component to add card data and implement edit functionality

--- App.tsx
+++ App.tsx
@@ -19,22 +19,23 @@ import {
   CardFooter,
 } from "./components/Card";
 import { Button } from "./components/Button";
-import { Dialog, DialogContent, DialogTrigger } from "./components/Dialog";
 
 export default function App() {
-  const [mainState, setMainState] = useState<"read" | "write">("read");
+  const [mainState, setMainState] = useState<"read" | "write" | "update">(
+    "read"
+  );
   const [editNoteId, setEditNoteId] = useState<number | undefined>(undefined);
 
   const cards = [
     {
-      title: "Accelerate UI",
-      description: "Enter a new development experience",
+      title: "Design System",
+      description: "Create a design system for your app",
       content: "Sleek, easy to use components to build your next app faster.",
       footer: "Inspired by shadcn/ui",
     },
     {
-      title: "Accelerate UI",
-      description: "Enter a new development experience",
+      title: "Design UX/UI",
+      description: "Create a design for the app",
       content: "Sleek, easy to use components to build your next app faster.",
       footer: "Inspired by shadcn/ui",
     },
@@ -62,50 +63,31 @@ export default function App() {
     return mainState === "read" ? (
       <SafeAreaView className="flex-1 pt-5 ">
         <ScrollView className="flex-1 ml-10">
-          <Dialog>
-            {cards.map((item, idx) => (
-              <DialogTrigger
-                onPress={() => {
-                  setEditNoteId(idx);
-                  alert("cliquei");
-                }}
-                key={idx}
-              >
-                <TouchableOpacity>
-                  <Card className="w-[90%] mt-4">
-                    <CardHeader>
-                      <CardTitle>{item.title}</CardTitle>
-                      <CardDescription>{item.description}</CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                      <Text className="text-base text-primary">
-                        {item.content}
-                      </Text>
-                    </CardContent>
-                    <CardFooter>
-                      <Text className="text-sm text-muted-foreground">
-                        {item.footer}
-                      </Text>
-                    </CardFooter>
-                  </Card>
-                </TouchableOpacity>
-              </DialogTrigger>
-            ))}
-            <DialogContent className="">
-              <View className="flex relative gap-4 bg-[#0694a2] p-12">
-                <TextInput
-                  value={
-                    editNoteId !== undefined ? cards[editNoteId].title : "oláaa"
-                  }
-                  className="w-full h-12 bg-white rounded-lg p-2"
-                />
-
-                <Text className="text-primary text-white">
-                  Tap outside the dialog to close it.
-                </Text>
-              </View>
-            </DialogContent>
-          </Dialog>
+          {cards.map((item, idx) => (
+            <TouchableOpacity
+              key={idx}
+              onPress={() => {
+                setEditNoteId(idx);
+                setMainState("update");
+                console.log(idx);
+              }}
+            >
+              <Card className="w-[90%] mt-4">
+                <CardHeader>
+                  <CardTitle>{item.title}</CardTitle>
+                  <CardDescription>{item.description}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <Text className="text-base text-primary">{item.content}</Text>
+                </CardContent>
+                <CardFooter>
+                  <Text className="text-sm text-muted-foreground">
+                    {item.footer}
+                  </Text>
+                </CardFooter>
+              </Card>
+            </TouchableOpacity>
+          ))}
         </ScrollView>
 
         <Button
@@ -115,17 +97,39 @@ export default function App() {
         />
         <StatusBar style="dark" />
       </SafeAreaView>
-    ) : (
-      <View>
+    ) : mainState === "write" ? (
+      <SafeAreaView className="flex-1 py-10 px-5">
         <Text>Write Note</Text>
         <TouchableOpacity
           onPress={() => setMainState("read")}
-          className="w-16 h-16 bg-[#0694a2] fixed top-5 right-5 text-white rounded-full"
+          className="w-12 h-12 bg-[#0694a2] absolute flex justify-center items-center top-8 right-5 text-white rounded-full"
         >
-          <Text>Close</Text>
+          <Text className="text-white text-lg">X</Text>
         </TouchableOpacity>
         <StatusBar style="dark" />
-      </View>
+      </SafeAreaView>
+    ) : (
+      <SafeAreaView className="flex-1 py-10 px-5">
+        <Text>Update Note</Text>
+        <TouchableOpacity
+          onPress={() => setMainState("read")}
+          className="w-12 h-12 bg-[#0694a2] absolute flex justify-center items-center top-8 right-5 text-white rounded-full"
+        >
+          <Text className="text-white text-lg">X</Text>
+        </TouchableOpacity>
+        <TextInput
+          placeholder="Title"
+          value={editNoteId !== undefined ? cards[editNoteId].title : ""}
+          className="border border-gray-400 p-2 mt-5"
+        />
+        <TextInput
+          placeholder="Description"
+          value={editNoteId !== undefined ? cards[editNoteId].description : ""}
+          className="border border-gray-400 p-2 mt-5"
+        />
+
+        <StatusBar style="dark" />
+      </SafeAreaView>
     );
   }
 }
